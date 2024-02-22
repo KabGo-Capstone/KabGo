fca7fcac852a3510f8b10a32fc41e0bbb19ddf7c
NG-MinG <wgmin.it@outlook.com>
2024-02-22 17:46:56 +0700

    config deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Parse command line arguments or use default values
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --host)
+            VPS_IP="$2"
+            shift 2
+            ;;
+        --uname)
+            USERNAME="$2"
+            shift 2
+            ;;
+        --port)
+            VPS_PORT="$2"
+            shift 2
+            ;;
+        --service)
+            SERVICE="$2"
+            shift 2
+            ;;
+        *)
+            echo "Invalid argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+rsync -avce ssh -p $VPS_PORT --delete ~/KabGo/servers/supply/dist "$USERNAME@$VPS_IP":/KabGo/servers/supply
+
+# Ensure all required parameters are provided
+if [[ -z $VPS_IP || -z $USERNAME || -z $VPS_PORT || -z $SERVICE ]]; then
+    echo "Usage: $0 --host <HOST> --uname <USERNAME> --port <VPS_PORT> --service <SERVICE>"
+    exit 1
+fi
+
+# SSH into the server and pull changes from the Git repository
+ssh -p $VPS_PORT "$USERNAME@$VPS_IP" "cd KabGo && git pull origin main"
+
+# Restart the application server (e.g., if using Node.js)
+ssh -p $VPS_PORT "$USERNAME@$VPS_IP" "cd KabGo/docker-compose && docker compose down && docker compose -f docker-compose-prod.yml up $SERVICE -d"
