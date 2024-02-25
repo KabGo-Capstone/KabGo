3bcbe34d67fd91a150d74bd5cee0eb2239c59496
NG-MinG <wgmin.it@outlook.com>
2024-02-26 05:15:27 +0700

    tesst2

--- servers/grpc/build.sh
+++ servers/grpc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROTO_DIR=./grpc/protos
-SOURCE_DIR=./grpc/proto_pb
+PROTO_DIR=protos
+SOURCE_DIR=proto_pb
 PROTOC_GEN_TS_PATH=./node_modules/.bin/protoc-gen-ts_proto
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"
 # PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
