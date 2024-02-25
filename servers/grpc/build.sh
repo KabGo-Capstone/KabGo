d99565d11566839c856fa48f9cbd33f9d6135653
NG-MinG <wgmin.it@outlook.com>
2024-02-26 05:27:48 +0700

    tesst2

--- servers/grpc/build.sh
+++ servers/grpc/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-PROTO_DIR=grpc/protos
-SOURCE_DIR=grpc/proto_pb
-PROTOC_GEN_TS_PATH=./node_modules/.bin/protoc-gen-ts_proto
-PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"
+PROTO_DIR=./protos
+SOURCE_DIR=./proto_pb
+# PROTOC_GEN_TS_PATH=./node_modules/.bin/protoc-gen-ts_proto
+# PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"
 # PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
 
 #Parse command line arguments or use default values
@@ -26,27 +26,24 @@ PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts_proto"
 
 npx grpc_tools_node_protoc \
     --grpc_out="grpc_js:${SOURCE_DIR}" \
-    --js_out=import_style=commonjs,binary:${SOURCE_DIR} \
+    --js_out="import_style=commonjs,binary:${SOURCE_DIR}" \
     --ts_out="grpc_js:${SOURCE_DIR}"  \
-    -I ./grpc/protos \
-    grpc/protos/*.proto
+    --proto_path "${PROTO_DIR}" \
+    -I ${PROTO_DIR} "${PROTO_DIR}/*.proto"
 
-# npx grpc_tools_node_protoc \
-#     --grpc_out="grpc_js:${SOURCE_DIR}" \
-#     --js_out="import_style=commonjs,binary:${SOURCE_DIR}" \
-#     --ts_out="grpc_js:${SOURCE_DIR}"  \
-#     --proto_path "${PROTO_DIR}" \
-#     "${PROTO_DIR}/*.proto"
-
-#Iterate over each file in the source directory
+# Iterate over each file in the source directory
 for file in "$SOURCE_DIR"/*; do
     if [[ -f "$file" ]]; then
         filename=$(basename -- "$file")
         extension="${filename##*.}"
         filename_no_ext="${filename%.*}"
-        folder="${filename%%_*}"  
 
-        mkdir -p $SOURCE_DIR/$folder
+        # Determine folder name dynamically
+        folder="${filename%%_*}"  # Extract the substring before the first underscore
+
+        if [[ ! -d "${SOURCE_DIR}/${folder}" && $folder != *.* ]]; then
+            mkdir -p "${SOURCE_DIR}/${folder}"
+        fi
 
         if [[ $filename == *_grpc_pb.* ]]; then
             mv "$file" "$SOURCE_DIR/$folder/${filename_no_ext}.${extension}"
